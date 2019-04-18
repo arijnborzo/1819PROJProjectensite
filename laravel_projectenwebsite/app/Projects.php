bdf3ca34cde3487a3e009a9cd596ef26647cd7db
Andreas Lauwers <andreas.lauwers@student.odisee.be>
2019-04-18 18:57:33 +0200

    Eloquent models finnished

--- laravel_projectenwebsite/app/Projects.php
+++ laravel_projectenwebsite/app/Projects.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Projects extends Model
 {
-    //
+    protected $fillable = ['Id', 'Title', 'Short_Description', 'Full_Description', 'Teacher_Id', 'Creator_Id',];
+    public function teachers(){
+        return $this->belongsTo('App\Teachers', 'Id', 'Teacher_Id');
+    }
+    public function users(){
+        return $this->belongsTo('App\Users', 'Id', 'Creator_Id');
+    }
+    public function groups(){
+        return $this->hasOne('App\Groups', 'Project_Id');
+    }
+    public function comments(){
+        return $this->hasMany('App\Comments', 'Project_Id');
+    }
+    public $timestamps = true;
 }
--- laravel_projectenwebsite/app/Teachers.php
+++ laravel_projectenwebsite/app/Teachers.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teachers extends Model
 {
-    //
+    protected $fillable = ['Id',];
+    public function users() {
+        return $this->hasOne('App\Users');
+    }
+    public function projects() {
+        return $this->hasMany('App\Projects');
+    }
 }
